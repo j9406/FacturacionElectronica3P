36ffe7ea0451f802a9dbace2e84fd0248606fc3d
j9406 <javier._94@hotmail.com>
2015-05-30 19:43:04 -0600

    CREACION DE BASE DE DATOS -Se creo una base de datos h2. -Se creo la clase de la base de datos.

--- FacturacionElectronicaProyecto/app/models/Facturaciondb.java
+++ FacturacionElectronicaProyecto/app/models/Facturaciondb.java
@@ -16,13 +16,36 @@ public class Facturaciondb extends Model {
     @Constraints.Min(10)
     public Long id;
 
-    @Constraints.Required
-    public String name;
+    @Constraints.Min(0)
+    @Constraints.Max(13)
+    @Constraints.Required(message="Debe ingresar el RFC del que expide")
+    public String RFCex;
 
-    public boolean done;
+    @Constraints.Required(message="Debe ingresar el numero de folio")
+    public int numFol;
+
+    @Constraints.Required(message="Debe ingresar el sello digital sat")
+    public String sell;
+
+    @Constraints.Required(message="Debe ingresar el lugar del que expide")
+    public String lugar;
 
     @Formats.DateTime(pattern="dd/MM/yyyy")
     public Date dueDate = new Date();
+
+    @Constraints.Min(0)
+    @Constraints.Max(13)
+    @Constraints.Required(message="Debe ingresar el RFC del que se expide")
+    public String RFCsexp;
+
+    @Constraints.Required(message="Debe ingresar descripción del producto")
+    public String desSer;
+
+    @Constraints.Required(message="Debe ingresar el valor unitario")
+    public int valUni;
+
+    @Constraints.Required(message="Debe ingresar el importe Total")
+    public float impTot;
 
     public static Finder<Long,Facturaciondb> find = new Finder<Long,Facturaciondb>(
             Long.class, Facturaciondb.class
